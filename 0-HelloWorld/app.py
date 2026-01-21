3fd5257a20fcb121d68501e313216cd3ee7c4a22
Suhagshetty <suhagshetty07@gmail.com>
2026-01-21 23:40:15 +0530

    Hello World and some variable declarations

---
+++ 0-HelloWorld/app.py
@@ -0,0 +1,2 @@
+print("Hello World")
+# Python is a interpreted language and its executed line by line.
---
+++ 1-Variables/app.py
@@ -0,0 +1,19 @@
+# Variable declaration
+price = 10
+price = 20
+print("The price is", price)
+
+# basic assignment
+name = "John Smith"
+age = 20
+isNewPatient = True
+print("The patients name is",name,"his age is",age,"he is a new patient",isNewPatient)
+
+# UserInput
+name = input("Whats your Name? ")
+print("So your name is ",name)
+
+# Assignment-2
+personName = input("Whats your Name:- ")
+personFavColor = input("Whats your fav color:- ")
+print(personName,"likes",personFavColor,"color")
