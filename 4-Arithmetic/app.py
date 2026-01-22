603b5aaccf7d80edcb2223346768702f8fb00908
Suhagshetty <suhagshetty07@gmail.com>
2026-01-22 11:28:29 +0530

    Math and modules

---
+++ 4-Arithmetic/app.py
@@ -0,0 +1,38 @@
+# Arithmetic Operators
+import math
+
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# Increment and decrements
+a+=1
+print(a)
+b-=5
+print(b)
+
+# Operator Precedence
+
+x = (10+3)*2**3
+print(x)
+
+# Parenthesis has the highest precedence followed by exponentiation (**) and multiplication * , division and later + or -
+
+
+# Math Modules in python
+
+x  = 2.9
+print(math.ceil(x))
+# rounds of the nearest large
+print(math.floor(x))
+# rounds off to the nearest low
+
+y = -1
+print(abs(y))
+# converts - to +
