b7d78dbeddada862827fad398074b96a8c7b0817
Suhagshetty <suhagshetty07@gmail.com>
2026-01-22 15:04:37 +0530

    end of loops

--- 6-Loops/app.py
+++ 6-Loops/app.py
@@ -13,3 +13,32 @@ else:
     print("You Lost!")
 
 # For Loops
+
+for name in ["Suhag","Shetty","Jake"]:
+    print(name)
+
+# looping through Numbers
+
+for numbers in [1,2,3,4,5]:
+    print(numbers)
+
+# Even Numbers printing
+for even in [1,2,3,4]:
+    if even % 2 == 0:
+        print("Even numbers here are, ",even)
+
+# To PRINT numbers from 0 to 10
+for tens in range(10):
+    print(tens)
+
+# STEPS FORWARD (x,y,"specify the gap ")
+for steps in range(6,10,2):
+    print(steps)
+
+# Looping through evens in 100
+for evens in range(100):
+   if evens%2==0:
+    print("The even numbesr from 1 to 100: ",evens)
+
+
+# NESTED LOOPS
